8b68874df4d40770eea61b319ba451ddb0b4f519
hyunjun9788 <namhyunjun9788@gmail.com>
2023-06-30 16:25:50 +0900

    feat: 단어 항목 추가

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
     const noSpace = (e) => {
         setText({
                 totalLength:e.target.value.length,
-                excludeSpace:e.target.value.replace(/\s/g, '').length
+                excludeSpace:e.target.value.replace(/\s/g, '').length,
+                word:e.target.value.split(' ').length
             }
         )
     }
@@ -19,6 +20,7 @@ function App() {
             <textarea className='text-box' onChange={noSpace}></textarea>
             <div>공백 제외:{text.excludeSpace}</div>
             <div>공백 포함:{text.totalLength}</div>
+            <div>단어:{text.word}</div>
         </div>
     );
 }
